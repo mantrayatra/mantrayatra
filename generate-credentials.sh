a35c29a73ddac612f79b8b43f0d65356d7a1e7c3
Kishan B <kishancs46@gmail.com>
2025-06-19 13:15:12 +0530

    Switch to maria db and upgrade all services

--- generate-credentials.sh
+++ generate-credentials.sh
@@ -6,6 +6,6 @@ if [[ -d credentials ]]; then
     echo "credentials already generated... skipping..."
 else
   mkdir credentials
-  uuidgen > credentials/mysql-root-user-password.txt
-  uuidgen > credentials/mysql-user-password.txt
+  uuidgen > credentials/db-root-user-password.txt
+  uuidgen > credentials/db-user-password.txt
 fi
